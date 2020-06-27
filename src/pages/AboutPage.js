73e8b363f9a9cfe380096861dcc3d2425bdad75a
hansa-lallu <hansalallu@gmail.com>
2020-06-27 07:36:17 +0100

    adding content to about me page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Hero from '../components/Hero'
+import Content from '../components/Content'
 
 function AboutPage(props) {
   return (
-    <p>About me!!!</p>
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <p>An ambitious Full-Stack Software Developer recently graduated from Makers, looking to join a company that embraces professional development. Previously worked as a Physiotherapist in New Zealand and London.</p>
+      </Content>
+    </div>
   );
 }
 

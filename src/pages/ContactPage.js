63d51185020cec50f25108873c544a24ab59b1ce
hansa-lallu <hansalallu@gmail.com>
2020-06-29 07:05:14 +0100

    creating a contact form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function ContactPage(props) {
-  return (
-    <p>Contact Me!</p>
-  );
+class ContactPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      disabled: false,
+      emailSent: null,
+    }
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      disabled: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Content>
+          <Form onSubmit={this.handleSubmit} name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full Name</Form.Label>
+              <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+            </Form.Group>
+
+
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+            </Form.Group>
+
+
+            <Form.Group>
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} />
+            </Form.Group>
+
+            <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+              Send
+            </Button>
+
+            {this.state.emailSent === true && <p className="d-inline success-msg font-weight-normal">Thanks for getting in touch, I will get back to you ASAP.</p>}
+            {this.state.emailSent === false && <p className="d-inline err-msg font-weight-normal">Error email not sent. Please try again.</p>}
+          </Form>
+        </Content>
+      </div>
+    );
+  }
 }
 
 export default ContactPage;

3c7fa4d86d6b65f10fd5ec6a00bd879bf6e647fb
hansa-lallu <hansalallu@gmail.com>
2020-06-30 09:15:37 +0100

    added button for linkedin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,21 @@
 import React from 'react';
 // import { Jumbotron } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function Contact(props) {
+
+  const styles = {
+    customButton: {
+      
+    }
+  }
+
   return (
     <div className="jumbotron-bg-img fluid p-0">
         <Row className="justify-content-center py-5"> 
             {props.contact && <h2 className="projects-title">{props.contact}</h2>}
+            <Button href='https://github.com/hansa-lallu/'></Button>
         </Row>
     </div>
   );

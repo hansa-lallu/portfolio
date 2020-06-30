6612f88452bafdcd5733c3aad3fe2174c7360d77
hansa-lallu <hansalallu@gmail.com>
2020-06-30 09:38:15 +0100

    added linkedin image link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
 // import { Jumbotron } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+import LinkedIn from '../assets/images/linkedin.png';
+
 
 function Contact(props) {
-
-  const styles = {
-    customButton: {
-      
-    }
-  }
-
   return (
-    <div className="jumbotron-bg-img fluid p-0">
-        <Row className="justify-content-center py-5"> 
+    <div className="jumbotron-bg-img fluid p-5">
+        <Row className="justify-content-center"> 
             {props.contact && <h2 className="projects-title">{props.contact}</h2>}
-            <Button href='https://github.com/hansa-lallu/'></Button>
+        </Row>
+        <Row className="justify-content-center">
+          <a href='https://www.linkedin.com/in/hansa-lallu'>
+            <img src={LinkedIn} width="100" alt="linkedin"></img>
+          </a>
         </Row>
     </div>
   );
